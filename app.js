6e33764bc8a5fc582d57d1f7f62638f8b63de2da
iRobot98 <matendejudah@gmail.com>
2023-05-30 16:07:29 +0300

    working on frontend

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
+const fs = require("fs")
 const favicon = require('serve-favicon')
 const path = require('path')
 const port = process.env.PORT || 3000
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-});
+app.get('/*', express.static("views/build"));
+app.get("/*",(req,res)=>{
+    res.type("html")
+    res.send(fs.readFileSync("./views/build/index.html"))
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
