de6845a3b3a560601fd87a622e4996bf58fd0fdb
iRobot98 <matendejudah@gmail.com>
2023-06-15 10:55:31 +0000

    woking on everything

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express()
 const fs = require("fs")
 const favicon = require('serve-favicon')
 const path = require('path')
-const port = process.env.PORT ? process.env.PORT:  3000
+const port = process.env.PORT ? process.env.PORT : 3000
 
 
 app.use(require("./src"))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.get('/*', express.static("views"));
+app.get('/*', express.static("views/build"));
+app.get("/", (req, res) => {
+    res.type("html");
+    res.send(fs.readFileSync("./views/build/index.html"))
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
